821e1505ede329a7e86a00b1e772dbc956d4584d
Jakub Pavlík <severus@post.cz>
2017-08-19 14:35:57 +0200

    "Na Signály chodím hlavně"

--- paper/graphs/graphs.R
+++ paper/graphs/graphs.R
@@ -9,11 +9,14 @@ write_answer_count <- function (responses, colname) {
     sink()
 }
 
+graph_path <- function (colname) paste("graphs/img/", colname, ".pdf", sep="")
+
 responses <- read.csv("../data/normalised.csv", na.strings=c(""))
 
+# koláčový graf rozložení odpovědí
 columns = list(
 "na_signalech_mam_profil", "o_signalech_jsem_se_poprve_dozvedel", "na_signaly_chodim",
-"na_signaly_chodim_hlavne", "signaly_jsem_vyuzil_k", "na_signalech_se_mi_povedlo", "jsem_autorizovany",
+"jsem_autorizovany",
 "na_signalech_jsem_zazil",
 "jsem", "vek", "studuji", "moje_nejvyssi_dosazene_vzdelani"
 )
@@ -25,27 +28,46 @@ for (colname in columns) {
     write_answer_count(responses, colname)
 
     # graf
-    path <- paste("graphs/img/", colname, ".pdf", sep="")
-    pdf(path)
+    pdf(graph_path(colname))
     frequency = sort(table(col))
     pie(frequency, col=rainbow(length(frequency)))
     dev.off()
 }
 
-# další sociální sítě: každý řádek může obsahovat několik hodnot
-colname <- "pouzivas_dalsi_socialni_site"
-write_answer_count(responses, colname)
-col <- responses[[colname]]
+# sloupcový graf, kažá odpověď může obsahovat víc položek
+columns <- list("pouzivas_dalsi_socialni_site", "signaly_jsem_vyuzil_k", "na_signalech_se_mi_povedlo")
 
-networks <- unlist(lapply(col, function (x) strsplit(as.character(x), ", ")))
+for (colname in columns) {
+    write_answer_count(responses, colname)
+    col <- responses[[colname]]
 
-path <- paste("graphs/img/", colname, ".pdf", sep="")
-pdf(path)
-frequency = sort(table(networks))
-par(las=2) # popisky horizontálně
-par(mar=c(5,8,4,2)) # okraje
-barplot(frequency, col=rainbow(length(frequency)), horiz=TRUE, cex.names=0.8)
-dev.off()
+    networks <- unlist(lapply(col, function (x) strsplit(as.character(x), ", ")))
+
+    pdf(graph_path(colname))
+    frequency = sort(table(networks))
+    par(las=2) # popisky horizontálně
+    par(mar=c(5,8,4,2)) # okraje
+    barplot(frequency, col=rainbow(length(frequency)), horiz=TRUE, cex.names=0.8)
+    dev.off()
+}
+
+# sloupcový graf, kažá odpověď může obsahovat víc položek,
+# některé položky obsahují čárky
+columns <- list("na_signaly_chodim_hlavne")
+
+for (colname in columns) {
+    write_answer_count(responses, colname)
+    col <- responses[[colname]]
+
+    networks <- unlist(lapply(col, function (x) strsplit(as.character(x), ",  "))) # čárka a dvě mezery, protože odpovědi často obsahují čárky
+
+    pdf(graph_path(colname))
+    frequency = sort(table(networks))
+    par(las=2) # popisky horizontálně
+    par(mar=c(5,20,4,2)) # okraje
+    barplot(frequency, col=rainbow(length(frequency)), horiz=TRUE, cex.names=0.8)
+    dev.off()
+}
 
 # vypsat varování
 warnings()
