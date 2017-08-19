95ca1ed3b714062b8af2617ccf6840621d438202
Jakub Pavlík <severus@post.cz>
2017-08-19 11:28:42 +0200

    "Používáš další sociální sítě?"
    + export graphs as pdf, not png

--- paper/graphs/graphs.R
+++ paper/graphs/graphs.R
@@ -5,7 +5,6 @@ responses <- read.csv("../data/normalised.csv", na.strings=c(""))
 columns = list(
 "na_signalech_mam_profil", "o_signalech_jsem_se_poprve_dozvedel", "na_signaly_chodim",
 "na_signaly_chodim_hlavne", "signaly_jsem_vyuzil_k", "na_signalech_se_mi_povedlo", "jsem_autorizovany",
-"pouzivas_dalsi_socialni_site",
 "na_signalech_jsem_zazil",
 "jsem", "vek", "studuji", "moje_nejvyssi_dosazene_vzdelani"
 )
@@ -19,9 +18,23 @@ for (colname in columns) {
     print(responseCount)
 
     # graf
-    path <- paste("graphs/img/", colname, ".png", sep="")
-    png(filename=path)
+    path <- paste("graphs/img/", colname, ".pdf", sep="")
+    pdf(path)
     frequency = sort(table(col))
     pie(frequency, col=rainbow(length(frequency)))
     dev.off()
 }
+
+# další sociální sítě: každý řádek může obsahovat několik hodnot
+colname <- "pouzivas_dalsi_socialni_site"
+col <- responses[[colname]]
+
+networks <- unlist(lapply(col, function (x) strsplit(as.character(x), ", ")))
+
+path <- paste("graphs/img/", colname, ".pdf", sep="")
+pdf(path)
+frequency = sort(table(networks))
+par(las=2) # popisky horizontálně
+par(mar=c(5,8,4,2)) # okraje
+barplot(frequency, col=rainbow(length(frequency)), horiz=TRUE, cex.names=0.8)
+dev.off()
